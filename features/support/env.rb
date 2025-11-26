7135da91ae5d73d14063e6d6e1a983bbe0278e25
vdeviv <revollovivian@gmail.com>
2025-11-26 00:36:21 -0330

    extras

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,8 +5,8 @@ require 'capybara/cucumber'
 require 'capybara-screenshot/cucumber'
 
 #PTravel Settings
-ENV['USER']="Pepazo"
-ENV['PSW']="ILoveQA"
+ENV['USER']="megapro"
+ENV['PSW']="IHateQA"
 
 Capybara.default_driver = :selenium
 
