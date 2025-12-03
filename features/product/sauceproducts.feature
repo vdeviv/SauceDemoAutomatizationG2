6d55e19e2790ed4d254059b29b5869273c4e4b1c
Arturo10021 <arturo.guzman@ucb.edu.bo>
2025-12-03 16:38:37 -0400

    Add product detail validation scenarios and step definitions

--- features/product/sauceproducts.feature
+++ features/product/sauceproducts.feature
@@ -10,3 +10,19 @@ Feature: Visualización de Productos
   @products_count
   Scenario: Validar que se muestren exactamente 6 productos
     Then debo ver un total de 6 productos en la página
+
+  Scenario: Validar detalles de productos con tabla de datos
+    Then veo los siguientes productos con sus detalles:
+      | nombre                  | precio | descripcion_parcial |
+      | Sauce Labs Backpack     | $29.99 | carry.allTheThings  |
+      | Sauce Labs Bike Light   | $9.99  | red light           |
+      | Sauce Labs Bolt T-Shirt | $15.99 | superhero           |
+
+  Scenario: Validar que las imágenes de los productos se carguen correctamente
+    Then todas las imágenes de los productos deben ser visibles
+
+  Scenario: Navegar al detalle del producto y volver
+    When hago click en el nombre del producto "Sauce Labs Bolt T-Shirt"
+    Then deberia estar en la página de detalles del producto "Sauce Labs Bolt T-Shirt"
+    When hago click en el botón para volver a productos
+    Then soy redirigido a la página de productos
--- features/step_definitions/sauce_products_steps.rb
+++ features/step_definitions/sauce_products_steps.rb
@@ -7,3 +7,42 @@ Then('debo ver un total de {int} productos en la página') do |expected_count|
   # Verificamos que el número de elementos encontrados sea exactamente el esperado (6)
   expect(product_items.count).to eq(expected_count)
 end
+
+Then('el producto {string} debe tener el precio {string}') do |nombre_producto, precio_esperado|
+  producto = find('.inventory_item', text: nombre_producto)
+  expect(producto).to have_css('.inventory_item_price', text: precio_esperado)
+end
+
+Then('el producto {string} debe tener la descripción {string}') do |nombre_producto, descripcion_esperada|
+  producto = find('.inventory_item', text: nombre_producto)
+  expect(producto).to have_css('.inventory_item_desc', text: descripcion_esperada)
+end
+
+Then('todas las imágenes de los productos deben ser visibles') do
+  imagenes = all('.inventory_item_img img')
+  imagenes.each do |img|
+    expect(img).to be_visible
+  end
+end
+
+When('hago click en el nombre del producto {string}') do |nombre_producto|
+  find('.inventory_item_name', text: nombre_producto).click
+end
+
+Then('deberia estar en la página de detalles del producto {string}') do |nombre_producto|
+  expect(page).to have_current_path(/inventory-item\.html/)
+  expect(page).to have_css('.inventory_details_name', text: nombre_producto)
+end
+
+When('hago click en el botón para volver a productos') do
+  find('#back-to-products').click
+end
+
+Then('veo los siguientes productos con sus detalles:') do |table|
+  datos = table.hashes
+  datos.each do |fila|
+    producto = find('.inventory_item', text: fila['nombre'])
+    expect(producto).to have_css('.inventory_item_price', text: fila['precio'])
+    expect(producto).to have_css('.inventory_item_desc', text: fila['descripcion_parcial'])
+  end
+end
