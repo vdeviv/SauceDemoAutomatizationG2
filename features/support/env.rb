8df4e749b286b2df0808a08ca398f651e72d3eec
vdeviv <revollovivian@gmail.com>
2025-12-09 00:31:42 -0330

    POM login

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,19 +5,16 @@ require 'capybara'
 require 'capybara/dsl'
 require 'capybara/cucumber'
 require 'capybara-screenshot/cucumber' 
-# Soporte para RSpec (incluye sintaxis antigua para compatibilidad)
+
 begin require 'rspec/expectations'; rescue LoadError; require 'spec/expectations'; end
 
 # --- Configuración de Variables de Entorno (Tomado de tu código) ---
 ENV['USER']="megapro"
 ENV['PSW']="ITRYLOVEQA"
 
-# CRÍTICO: Añadir la carpeta 'pages' al load path global
-# Esto permite usar require 'nav_menu_page' sin rutas relativas complejas.
-# Se asume que 'pages' está al mismo nivel que 'support' (en la raíz del proyecto).
+
 $LOAD_PATH << File.expand_path('../pages', __FILE__)
 
-# Extiende el contexto de Cucumber para incluir los métodos de Capybara (CRÍTICO)
 World(Capybara::DSL)
 World(RSpec::Matchers)
 
