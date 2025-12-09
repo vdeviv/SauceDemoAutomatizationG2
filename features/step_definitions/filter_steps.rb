97442feff0c4d9f9fa3fd2fd6ebc055fe40c85ff
Arturo10021 <arturo.guzman@ucb.edu.bo>
2025-12-09 07:44:45 -0400

    refactor sorting validation to use visible items directly from the page

--- features/step_definitions/filter_steps.rb
+++ features/step_definitions/filter_steps.rb
@@ -13,31 +13,32 @@ end
 
 Then('los productos deberían ordenarse por {string}') do |criterio|
   
+  items = all('.inventory_item')
+  
   case criterio
   when 'nombre_asc'
-
-    nombres_ui = @filter_page.get_visible_product_names 
+    nombres_ui = items.map { |item| item.find('.inventory_item_name').text }
     nombres_esperados = nombres_ui.sort
     
-    expect(nombres_ui).to eq(nombres_esperados), "Los nombres no están ordenados de A a Z."
+    expect(nombres_ui).to eq(nombres_esperados), "La lista visual de productos no está ordenada alfabéticamente (A-Z)."
 
   when 'nombre_desc'
-    nombres_ui = @filter_page.get_visible_product_names 
+    nombres_ui = items.map { |item| item.find('.inventory_item_name').text }
     nombres_esperados = nombres_ui.sort.reverse
     
-    expect(nombres_ui).to eq(nombres_esperados), "Los nombres no están ordenados de Z a A."
+    expect(nombres_ui).to eq(nombres_esperados), "La lista visual de productos no está ordenada alfabéticamente (Z-A)."
 
   when 'precio_asc'
-    precios_ui = @filter_page.get_visible_product_prices
+    precios_ui = items.map { |item| item.find('.inventory_item_price').text.gsub('$', '').to_f }
     precios_esperados = precios_ui.sort
     
-    expect(precios_ui).to eq(precios_esperados), "Los precios no están ordenados de menor a mayor."
+    expect(precios_ui).to eq(precios_esperados), "La lista visual de precios no está ordenada de menor a mayor."
 
   when 'precio_desc'
-    precios_ui = @filter_page.get_visible_product_prices
+    precios_ui = items.map { |item| item.find('.inventory_item_price').text.gsub('$', '').to_f }
     precios_esperados = precios_ui.sort.reverse
     
-    expect(precios_ui).to eq(precios_esperados), "Los precios no están ordenados de mayor a menor."
+    expect(precios_ui).to eq(precios_esperados), "La lista visual de precios no está ordenada de mayor a menor."
 
   else
     raise "Criterio de ordenamiento no reconocido: #{criterio}"
